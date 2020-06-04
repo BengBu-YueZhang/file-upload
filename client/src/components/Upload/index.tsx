c99b569c48a3297311948a1788ad98592462db2d
BengBu-YueZhang <zhangyue_coder@163.com>
2020-06-04 16:52:37 +0800

    fead: 更新upload组件

--- client/src/components/Upload/index.tsx
+++ client/src/components/Upload/index.tsx
@@ -8,7 +8,7 @@ import http from '../../utils/http';
 import noop from '../../utils/noop';
 import uuid from '../../utils/uuid';
 
-enum Status {
+enum UploadStatus {
   Uploading,
   Done,
   Error
@@ -37,12 +37,17 @@ export interface IFile {
   uid: string;
   size: number;
   name: string;
-  status: Status;
+  status: UploadStatus;
   response: JSON | null;
   progress: number;
 };
 
 export interface IChunk {
+  blob: Blob;
+  name: string;
+  index: number;
+  status: UploadStatus;
+  progress: number;
 };
 
 const Upload: React.FC<IUpload> = (props) => {
@@ -70,19 +75,20 @@ const Upload: React.FC<IUpload> = (props) => {
   const inputFileEl = useRef(null);
   const inputFileElId = useMemo(() => uuid(), []);
   const isLarge = useMemo(() => large || breakpointResume, [large, breakpointResume]);
+  const isBreakpointResume;
   // 文件列表
   const [fileList, setFileList] = useState<IFile[]>([]);
   // chunk列表
-  const [chunkList, setChunkList] = useState([]);
+  const [chunkList, setChunkList] = useState<IChunk[]>([]);
   // 使用两个数组，做并发上传的限制
   // 文件待上传队列
   const [fileQueue, setFileQueue] = useState<IFile[]>([]);
   // 正在上传的文件队列
   const [uploadFileQueue, setUploadFileQueue] = useState<IFile[]>([]);
   // chunk待上传队列
-  const [chunkQueue, setChunkQueue] = useState([]);
+  const [chunkQueue, setChunkQueue] = useState<IChunk[]>([]);
   // 正在上传的chunk队列
-  const [uploadChunkQueue, setUploadChunkQueue] = useState([]);
+  const [uploadChunkQueue, setUploadChunkQueue] = useState<IChunk[]>([]);
 
   // 如果是大文件上传，必须是单选
   if (isLarge && multiple) {
@@ -104,6 +110,17 @@ const Upload: React.FC<IUpload> = (props) => {
   }
 
   const handleLargeFile = (file: File) => {
+    const fileItem = {
+      file: file,
+      uid: uuid(),
+      size: file.size,
+      name: file.name,
+      status: UploadStatus.Uploading,
+      response: null,
+      progress: 0
+    };
+    setFileList(prevFileList => [...prevFileList, fileItem]);
+    fileSlicing(file);
   };
 
   const handleSmallFile = (files: FileList) => {
@@ -117,7 +134,7 @@ const Upload: React.FC<IUpload> = (props) => {
           uid: uuid(),
           size: files[i].size,
           name: files[i].name,
-          status: Status.Uploading,
+          status: UploadStatus.Uploading,
           response: null,
           progress: 0
         };
@@ -137,13 +154,65 @@ const Upload: React.FC<IUpload> = (props) => {
       uploadFileQueue.length < (concurrency as number) &&
       fileQueue.length > 0
     ) {
+      const awaitUploadFile = fileQueue[0];
+      setFileQueue(prevFileQueue => {
+        prevFileQueue.shift();
+        return [...prevFileQueue];
+      });
+      setUploadFileQueue(prevUploadFileQueue => [...prevUploadFileQueue, awaitUploadFile]);
+      submitFileQueue();
     }
   };
 
-  const addChunkQueue = () => {
+  const submitFileQueue = () => {
+  };
+
+  // 大文件切片
+  const fileSlicing = (file: File) => {
+    const name = file.name;
+    if (file.size > (chunkSize as number)) {
+      let startPointer = 0;
+      let endPointer = 0;
+      let index = -1;
+      while (true) {
+        startPointer += chunkSize as number;
+        const blob = file.slice(startPointer, endPointer);
+        startPointer = endPointer;
+        index += 1;
+        if (!blob.size) {
+          break;
+        }
+        const chunkItem = {
+          blob,
+          name,
+          index,
+          status: UploadStatus.Uploading,
+          progress: 0
+        };
+        setChunkList(prevChunkList => [...prevChunkList, chunkItem]);
+        addChunkQueue(chunkItem);
+      }
+    } else {
+      const blob = file.slice(0);
+      const chunkItem = {
+        blob,
+        name,
+        index: 0,
+        status: UploadStatus.Uploading,
+        progress: 0
+      };
+      setChunkList(prevChunkList => [...prevChunkList, chunkItem]);
+      addChunkQueue(chunkItem);
+    }
+  };
+
+  const addChunkQueue = (chunk: IChunk) => {
   };
 
   const flushChunkQueue = () => {
+  };
+
+  const submitChunkQueue = () => {
   };
 
   return (
