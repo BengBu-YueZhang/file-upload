d0ff78fa7950312950da8be38c140d7001d65cd9
BengBu-YueZhang <zhangyue_coder@163.com>
2020-06-10 17:21:27 +0800

    fead: 更新自定义hook

--- client/src/hooks/useStateWithCallback.tsx
+++ client/src/hooks/useStateWithCallback.tsx
@@ -2,19 +2,20 @@ import React, { useState, useEffect, useRef } from 'react';
 
 function useStateWithCallback<T>(
   initialState: T | (() => T)
-): [T, (value: React.SetStateAction<T>, callback?: (() => void) | undefined) => void] {
-  const setStateCallback = useRef<(() => void) | undefined>(undefined);
+): [T, (value: React.SetStateAction<T>, callback?: ((state: T) => void) | undefined) => void] {
+
+  const setStateCallback = useRef<((state: T) => void) | undefined>(undefined);
   const [state, setState] = useState<T>(initialState);
   const setStateWrapper = function (
     value: React.SetStateAction<T>,
-    callback?: (() => void) | undefined
+    callback?: ((state: T) => void) | undefined
   ): void {
     setStateCallback.current = callback;
     setState(value);
   };
 
   useEffect(function () {
-    setStateCallback.current && setStateCallback.current();
+    setStateCallback.current && setStateCallback.current(state);
   }, [state]);
 
   return [
